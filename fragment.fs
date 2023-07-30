184755e453318842bda5db562745c9f438d4c9c5
Umar Nawed <umar.nawed@gmail.com>
2023-07-30 13:50:24 -0400

    Debug stuff

--- fragment.fs
+++ fragment.fs
@@ -2,6 +2,8 @@
 
 out vec4 FragColor;
 
+uniform vec3 uColor;
+
 void main() {
-	FragColor = vec4(1.0f,0.0f,0.0f, 1.0f);
+	FragColor = vec4(uColor, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 const float RESOLUTION = 100.0f;
-const float UNIT_SIZE = 0.01f;
+const float UNIT_SIZE = 1.0f;
 
 const unsigned int NUM_BOX_X = (unsigned int) RESOLUTION;
 const unsigned int NUM_BOX_Y = (unsigned int) RESOLUTION;
@@ -29,40 +29,91 @@ std::vector<float> vertices;
 
 glm::vec4 cubePositions[8] = {
 	//bottom vertices
-	glm::vec4(-1.0f, -1.0f, -1.0f, 1.0f),
-	glm::vec4(1.0f, -1.0f, -1.0f, 1.0f),
-	glm::vec4(1.0f, -1.0f, 1.0f, 1.0f),
-	glm::vec4(-1.0f, -1.0f, 1.0f, 1.0f),
+	glm::vec4(-0.5f, -0.5f, 0.5f, 1.0f),
+	glm::vec4(0.5f, -0.5f, 0.5f, 1.0f),
+	glm::vec4(0.5f, -0.5f, -0.5f, 1.0f),
+	glm::vec4(-0.5f, -0.5f, -0.5f, 1.0f),
 	//Top vertices
-	glm::vec4(-1.0f, 1.0f, -1.0f, 1.0f),
-	glm::vec4(1.0f, 1.0f, -1.0f, 1.0f),
-	glm::vec4(1.0f, 1.0f, 1.0f, 1.0f),
-	glm::vec4(-1.0f, 1.0f, 1.0f, 1.0f),
+	glm::vec4(-0.5f, 0.5f, 0.5f, 1.0f),
+	glm::vec4(0.5f, 0.5f, 0.5f, 1.0f),
+	glm::vec4(0.5f, 0.5f, -0.5f, 1.0f),
+	glm::vec4(-0.5f, 0.5f, -0.5f, 1.0f),
 };
 
 // Abstract to an object so we can have other shapes
 bool isInSphere(glm::vec3 vertex) {
-	return glm::length(vertex) < 0.25f;
+	return glm::length(vertex) < 1.0f;
 };
 
+struct VAOs {
+	GLuint objVao;
+	GLuint gridVao;
+	int numOfGridElements;
+};
 
 //Unoptimized and glitchy, can create spheres-like object with the current configs
-GLuint createSphereBuffer() {
+VAOs createSphereBuffer() {
+	std::cout << glm::length(glm::vec3(0.5f, 0.5f, 0.5f)) << std::endl;
+
+	std::vector<float> cubeVerticesBuffer;
+	std::vector<unsigned int> cubeElementBuffer;
+
+	float cubeWidth = UNIT_SIZE ;
 	glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(UNIT_SIZE));
 	int item_count = 0;
-	for(float x=-1.0f; x<1.0f; x+=UNIT_SIZE) {
-		for(float y=-1.0f; y<1.0f; y+=UNIT_SIZE) {
-			for(float z=-1.0f; z<1.0f; z+=UNIT_SIZE) {
+	for(float x=-1.0f; x<=1.0f; x+=cubeWidth) {
+		for(float y=-1.0f; y<=1.0f; y+=cubeWidth) {
+			for(float z=-1.0f; z<=1.0f; z+=cubeWidth) {
+				int start_el = (cubeVerticesBuffer.size() / 3);
+				int start_v = cubeVerticesBuffer.size();
 				uint8_t code = 0;
 				uint8_t curr_vertex = 1;
 				glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(x,y,z));
 				for (glm::vec4 cvertex: cubePositions) {
 					glm::vec4 newPos = translate * scale * cvertex;
-					if (isInSphere(glm::vec3(newPos.x, newPos.y, newPos.z))) {
+					cubeVerticesBuffer.push_back(newPos.x);
+					cubeVerticesBuffer.push_back(newPos.y);
+					cubeVerticesBuffer.push_back(newPos.z);
+
+					float len = glm::length(glm::vec3(newPos.x, newPos.y, newPos.z));
+					std::cout << "(" << newPos.x << ", " << newPos.y << ", " << newPos.z << ") --> " << len << ":" << std::endl;
+					
+					if (!isInSphere(glm::vec3(newPos.x, newPos.y, newPos.z))) {
 						code |= curr_vertex;
 					}
 					curr_vertex = curr_vertex << 1;
 				}
+				// Bottom
+				cubeElementBuffer.push_back(start_el + 0); cubeElementBuffer.push_back(start_el + 1); cubeElementBuffer.push_back(start_el + 3);
+				cubeElementBuffer.push_back(start_el + 1); cubeElementBuffer.push_back(start_el + 2); cubeElementBuffer.push_back(start_el + 3);
+
+				// Front
+				cubeElementBuffer.push_back(start_el + 0); cubeElementBuffer.push_back(start_el + 1); cubeElementBuffer.push_back(start_el + 4);
+				cubeElementBuffer.push_back(start_el + 1); cubeElementBuffer.push_back(start_el + 5); cubeElementBuffer.push_back(start_el + 4);
+
+				// Back
+				cubeElementBuffer.push_back(start_el + 3); cubeElementBuffer.push_back(start_el + 2); cubeElementBuffer.push_back(start_el + 7);
+				cubeElementBuffer.push_back(start_el + 2); cubeElementBuffer.push_back(start_el + 6); cubeElementBuffer.push_back(start_el + 7);
+
+				// Top
+				cubeElementBuffer.push_back(start_el + 4); cubeElementBuffer.push_back(start_el + 5); cubeElementBuffer.push_back(start_el + 7);
+				cubeElementBuffer.push_back(start_el + 5); cubeElementBuffer.push_back(start_el + 6); cubeElementBuffer.push_back(start_el + 7);
+
+				// Left
+				cubeElementBuffer.push_back(start_el + 0); cubeElementBuffer.push_back(start_el + 4); cubeElementBuffer.push_back(start_el + 7);
+				cubeElementBuffer.push_back(start_el + 0); cubeElementBuffer.push_back(start_el + 7); cubeElementBuffer.push_back(start_el + 3);
+
+				// Right
+				cubeElementBuffer.push_back(start_el + 1); cubeElementBuffer.push_back(start_el + 2); cubeElementBuffer.push_back(start_el + 6);
+				cubeElementBuffer.push_back(start_el + 1); cubeElementBuffer.push_back(start_el + 6); cubeElementBuffer.push_back(start_el + 5);
+
+
+				for (int i = start_v; i < start_v + 24; i += 3)
+				{
+					float len = glm::length(glm::vec3(cubeVerticesBuffer[i], cubeVerticesBuffer[i+1], cubeVerticesBuffer[i+2]));
+			//		std::cout << "(" << cubeVerticesBuffer[i] << ", " << cubeVerticesBuffer[i+1] << ", " << cubeVerticesBuffer[i+2] << ") --> " << len << std::endl;
+				}
+				std::cout << "(" << x << ", " << y << ", " << z << ") --> " << (int) code << std::endl;
 				std::vector<float> unitVertices = getVertices(code);
 				if (code != 0) {
 					for (int i=0; i < unitVertices.size(); i+=3) {
@@ -91,7 +142,25 @@ GLuint createSphereBuffer() {
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-	return VAO;
+
+	GLuint cubeVao;
+	glGenVertexArrays(1, &cubeVao);
+	glBindVertexArray(cubeVao);
+	
+	GLuint cubeVertexBuffer;
+	glGenBuffers(1, &cubeVertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*cubeVerticesBuffer.size(), cubeVerticesBuffer.data(), GL_STATIC_DRAW);
+
+	GLuint cubeElementsBuffer;
+	glGenBuffers(1, &cubeElementsBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeElementsBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * cubeElementBuffer.size(), cubeElementBuffer.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+	return { VAO, cubeVao, cubeElementBuffer.size() };
 };
 
 // camera
@@ -106,7 +175,6 @@ float lastFrame = 0.0f;
 
 uint8_t config_num = 1;
 std::vector<float> cubeVector(12);
-GLuint mVAO;
 
 GLuint createBuffers()
 {
@@ -161,14 +229,15 @@ int main() {
 	}
 	gen_table();
 
-	mVAO = createSphereBuffer();
+	VAOs vaos = createSphereBuffer();
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     glEnable(GL_DEPTH_TEST);
 
 	Shader shader("vertex.vs", "fragment.fs");
 	shader.use();
+
+	std::cout << camera.Position.x << camera.Position.y << camera.Position.z << std::endl;
 
 	while(!glfwWindowShouldClose(window)) {
 
@@ -189,8 +258,20 @@ int main() {
 		glm::mat4 view = camera.GetViewMatrix();
 		shader.setMat4("view", view);
 		
-		glBindVertexArray(mVAO);
+		shader.setVec3("uColor", glm::vec3(0.0, 1.0, 0.0));
+		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+		glBindVertexArray(vaos.gridVao);
+		glDrawElements(GL_TRIANGLES, vaos.numOfGridElements, GL_UNSIGNED_INT, nullptr);
+		
+		shader.setVec3("uColor", glm::vec3(1.0, 0.0, 0.0));
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glBindVertexArray(vaos.objVao);
 		glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+		shader.setVec3("uColor", glm::vec3(1.0, 0.0, 1.0));
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glBindVertexArray(vaos.objVao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -213,7 +294,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 uint32_t lastChangeTime = 0;
 void processInput(GLFWwindow *window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
@@ -224,12 +305,12 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && lastChangeTime == 0)
-	{
-		config_num += 1;
-		mVAO = createBuffers();
-		lastChangeTime = 15;
-	}
+	//if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && lastChangeTime == 0)
+	//{
+		//config_num += 1;
+		//mVAO = createBuffers();
+		//lastChangeTime = 15;
+	//}
 
 	if (lastChangeTime) lastChangeTime -= 1;
 }
